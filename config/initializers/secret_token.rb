7524a9f309ae58b6a10e579ff42848d47aa986da
Danny Wu <wushuo1988@hotmail.com>
2014-04-05 16:55:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoupToBits::Application.config.secret_key_base = '4b764154ce66426512a22f7a5a123e572881660d991c2efced45ba95d8048b1d0a646475ab993e8b791628f6a02679cb29708866434de50c19d8231b1995eafc'
